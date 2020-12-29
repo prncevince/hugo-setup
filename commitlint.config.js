e6e0e275c24e63d5668d72ae4ea5d7e4bd3c80d0
Vincent Clemson <vclemson07@gmail.com>
2020-12-28 23:33:06 -0500

    feat(setup): initialize lerna and conventional changelog's commitlint use husky and commitiquette for git commits

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.']
+  }
+}
